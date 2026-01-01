46525a7db43ce880df1e2941ad78ce6d75c15f52
nabilalutfiasari313-dot <nabilalutfiasari313@gmail.com>
2026-01-01 23:29:35 +0700

    Add files via upload

---
+++ AlatMusik.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Polimorfisme;
+
+/**
+ *
+ * @author nabil
+ */
+public class AlatMusik {
+    public void bunyi() {
+        System.out.println("Setiap alat musik memiliki bunyi yang berbeda.");
+    }
+}
